62ecce406e3d715ff94419adbbe169a4cf7015f8
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2020-07-09 17:45:29 +0200

    Initial Commit

---
+++ DailyProgrammer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DailyProgrammer
+//
+//  Created by Grzegorz Gumieniak on 09/07/2020.
+//  Copyright © 2020 Grzegorz Gumieniak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
