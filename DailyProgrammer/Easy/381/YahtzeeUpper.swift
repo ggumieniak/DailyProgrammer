7e3b94b127534f05f99def8eee8e37d8a30bdd44
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2020-07-10 12:14:01 +0200

    Start new easy challenge

---
+++ DailyProgrammer/Easy/381/YahtzeeUpper.swift
@@ -0,0 +1,13 @@
+//
+//  YahtzeeUpper.swift
+//  DailyProgrammer
+//
+//  Created by Grzegorz Gumieniak on 10/07/2020.
+//  Copyright © 2020 Grzegorz Gumieniak. All rights reserved.
+//
+
+import Foundation
+
+struct YahtzeeUpper {
+    
+}
